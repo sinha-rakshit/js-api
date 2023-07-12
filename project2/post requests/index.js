ce87171629fdefc46159ddacf3057bd2c292e520
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-07-12 19:16:59 +0530

    added a database

--- project2/post requests/index.js
+++ project2/post requests/index.js
@@ -1,16 +1,25 @@
 const express = require("express");
+const Datastore = require("nedb");
+
 const app = express();
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+const database = new Datastore("local_data.db");
+database.loadDatabase();
+
 app.post("/api", (request, response) => {
   console.log("I got a request");
-  console.log(request.body);
-
   const data = request.body;
+
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+
   response.json({
     status: "success",
+    Time: timestamp,
     latitude: data.Lat,
     longitude: data.Lng,
   });
