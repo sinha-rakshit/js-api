32740b20c956a402a2a98dea4e51823ef805e744
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-07-12 18:39:30 +0530

    added server but not understood much

---
+++ project2/post requests/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("Listening at 3000"));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/api", (request, response) => {
+  console.log("I got a request");
+  console.log(request.body);
+
+  const data = request.body;
+  response.json({
+    status: "success",
+    latitude: data.Lat,
+    longitude: data.Lng,
+  });
+});
